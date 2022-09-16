fa3c51d63c9a2e60e4d899db142be8ebf7e2e88e
Edgar <32952693+Yongyiphan@users.noreply.github.com>
2022-09-17 01:37:01 +0800

    Fade in and out using sine wave

--- SplashScreen/Src/splashscreen.c
+++ SplashScreen/Src/splashscreen.c
@@ -4,8 +4,8 @@
 #include <math.h>
 
 int BG_Width, BG_Height, m, printed;
-int frame, animationSecs, runtime;
-float alpha, TransitionSpeed;
+int transparency, runtime;
+float alpha, periodPerFrame, PI = 22.0/7.0;
 float TotalElapsedTime;
 
 CP_Image BG;
@@ -17,34 +17,39 @@ void splash_screen_init(void)
 	CP_System_SetWindowSize(CP_Image_GetWidth(BG), CP_Image_GetHeight(BG));
 
 	//transparency value
-	runtime = 2;
+	runtime = 5;
 
 }
 void splash_screen_update(void)
 {
-	//Flutter
-	int currentFps = CP_System_GetFrameRate();
-	int currentFrameCount = CP_System_GetFrameCount();
-	int fps_cycle = (int)CP_System_GetFrameRate() * runtime;
-	int kek = currentFrameCount % fps_cycle;
-	int TimeTakenSec = (int) floor(TotalElapsedTime) % runtime;
+	////Fluff
+	//int currentFps = CP_System_GetFrameRate();
+	//int currentFrameCount = CP_System_GetFrameCount();
+	//int fps_cycle = (int)CP_System_GetFrameRate() * runtime;
+	//int kek = currentFrameCount % fps_cycle;
+	//int TimeTakenSec = (int) floor(TotalElapsedTime) % runtime;
 	
 	//Impt codes
+	CP_Graphics_ClearBackground(CP_Color_Create(10,10,10,255));
 	//Fade in
 	//tl:dr math =
 	//Get perc of (time taken from last to current frame) from (Specified Runtime)
 	//Use perc to get alpha's perc equivalent => use this as transition speed to next frame
-	//Rinse and repeat.
-	CP_Graphics_ClearBackground(CP_Color_Create(10,10,10,255));
-	TotalElapsedTime += CP_System_GetDt();
-	alpha += (CP_System_GetDt() / runtime) * 255;
-	//Resets img alpha value to 0 after max value of 255
-	m = (int) alpha % 255;
-	CP_Image_Draw(BG, 10.f, 10.f,CP_System_GetWindowWidth(), CP_System_GetWindowHeight(), m);
+
+	//alpha += (CP_System_GetDt() / runtime) * 255;
+	////Resets img alpha value to 0 after max value of 255
+	//m = (int) alpha % 255;
+	//CP_Image_Draw(BG, 10.f, 10.f,CP_System_GetWindowWidth(), CP_System_GetWindowHeight(), m);
 	
 	//Fade in AND Fade out
 	//tl:dr math = 
-	int transparency = runtime * sin(TotalElapsedTime);
+	//using sine wave for oscillating values between 0 - 255
+	//1 cycle from 0-255 is ~ PI 
+	
+	//TotalElapsedTime += CP_System_GetDt();
+	//alpha = abs(255 * sinf(TotalElapsedTime * PI/runtime));
+	//printf("%f | %f | %f | %f\n", alpha, TotalElapsedTime, PI, periodPerFrame);
+	//CP_Image_Draw(BG, 10.f, 10.f, CP_System_GetWindowWidth(), CP_System_GetWindowHeight(), (int) alpha);
 
 
 
