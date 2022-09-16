0c01ffcd2c33dca6af9dd7d5916107ba8f98c13b
Edgar <edgar.yong.yip.han@gmail.com>
2022-09-16 19:14:45 +0800

    init Car Proj

---
+++ Car/Src/carlevel.c
@@ -0,0 +1,16 @@
+
+
+void Car_Level_Init()
+{
+	
+}
+
+void Car_Level_Update()
+{
+	
+}
+
+void Car_Leve_Exit()
+{
+
+}
---
+++ Car/Src/mainmenu.c
@@ -0,0 +1,16 @@
+
+
+void Main_Menu_Init()
+{
+
+}
+
+void Main_Menu_Update()
+{
+
+}
+
+void Main_Menu_Exit()
+{
+
+}
